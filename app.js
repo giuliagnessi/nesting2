a59fb40449ecb9e378bdac9ca894658adea5d9c2
Julie <113455252+giuliagnessi@users.noreply.github.com>
2025-05-21 17:27:13 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
-
-export async function handler(req) {
-  return serveDir(req, {
-    fsRoot: "./dist",
-    urlRoot: "",
-    showDirListing: true,
-  });
-}
